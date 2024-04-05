7cd5a7f5b26d7bceeb0ccd9ea384dc0b9b6a060b
정지후 <jjh123579@snu.ac.kr>
2024-04-05 11:10:54 +0900

    Improve acccelerating

--- lipnet.py
+++ lipnet.py
@@ -11,39 +11,44 @@ import torch.nn.functional as F
 import torch.nn.init as init
 from tqdm import tqdm
 from torch.optim import Adam
-from accelerate import Accelerator
+from accelerate import (Accelerator, DistributedDataParallelKwargs,
+                        DistributedType)
+
 import wandb
 from ctcdecode import CTCBeamDecoder
+from accelerate.tracking import WandBTracker
+from accelerate.utils import DistributedDataParallelKwargs, InitProcessGroupKwargs
+from datetime import timedelta
+from contextlib import contextmanager, nullcontext
 
+
+DEFAULT_DDP_KWARGS = DistributedDataParallelKwargs(find_unused_parameters = True)
+
+def accum_log(log, new_logs):
+    for key, new_value in new_logs.items():
+        old_value = log.get(key, 0.)
+        log[key] = old_value + new_value
+    return log
 class CustomDataset(Dataset): 
     def __init__(self):
-        # self.video_list=[]
-        # for i in range(1,35):
-            
-        #     new_videos=glob.glob(f'/dataset/grid/s{i}/*.mpg')
-        #     if len(new_videos)==0:
-        #         print("continuew on ",i)
-        #         continue
-        #     new_videos=sorted(new_videos)
-        #     new_align = glob.glob(f'/dataset/grid/alignments/s{i}/*.align')
-        #     new_align=sorted(new_align)
-            
-        #     if new_videos[0].split("/")[-1].split(".")[0]!=new_align[0].split("/")[-1].split(".")[0]:
-        #         print(i,"\t",new_videos[0],new_align[0])
-            
-            
-        self.video_list = glob.glob('/dataset/grid/**/*.mpg', recursive=True)
+        
+        # self.video_list = glob.glob('/dataset/grid/**/*.mpg', recursive=True)
+        # self.video_list=sorted(self.video_list)
+        # self.align_list = glob.glob('/dataset/grid/alignments/**/*.align',recursive=True)
+        # self.align_list=sorted(self.align_list)
+        
+        self.video_list = glob.glob('/dataset/grid/s1/*.mpg')
         self.video_list=sorted(self.video_list)
-        self.align_list = glob.glob('/dataset/grid/alignments/**/*.align',recursive=True)
+        self.align_list = glob.glob('/dataset/grid/alignments/s1/*.align')
         self.align_list=sorted(self.align_list)
         
         assert len(self.align_list)==len(self.video_list), f"{len(self.align_list)}, {len(self.video_list)} 둘의 길이가 일치하지 않음."
+        
+        '''ensure that everthing is well aligned'''
         for i, (video_path, align_path) in enumerate(zip(self.video_list, self.align_list)):
             # os.path를 사용하여 파일 이름 추출
             video = os.path.splitext(os.path.basename(video_path))[0]
             align = os.path.splitext(os.path.basename(align_path))[0]
-
-            # 파일 이름이 일치하지 않는 경우 인덱스 출력
             if video != align:
                 print(i,video_path, align_path)
         print("done loading the dataset")
@@ -56,7 +61,7 @@ class CustomDataset(Dataset):
         alignments =self.load_alignments(idx)
         assert frames.shape[0]==75, f"75 아니고 {frames.shape[0]}"
         assert frames.shape[1]==1, f"1 아니고 {frames.shape[1]}"
-        assert alignments.shape[0]==50, f"50 아니고 {alignments.shape[0]}"
+        assert alignments.shape[0]==30, f"50 아니고 {alignments.shape[0]}"
         return frames,alignments
     
     def load_video(self, idx):
@@ -100,7 +105,7 @@ class CustomDataset(Dataset):
 
         # 문자열을 숫자로 변환
         num_tokens = char_to_num(tokens[1:])  # 첫 번째 공백을 제외하고 변환
-        target_length = 50
+        target_length = 30
         current_length = num_tokens.size(0)
         if current_length < target_length:
             # (앞쪽 패딩, 뒤쪽 패딩) 순서로 패딩을 추가
@@ -183,7 +188,20 @@ def save_model(model,save_path):
     torch.save(model.state_dict(), save_path)
     print(save_path," 저장하였습니다.")
     
+def load(model, accelerator,model_path, device):
+        model_state_dict = torch.load(model_path, map_location=device)
+        model = accelerator.unwrap_model(model)
+        model.load_state_dict(model_state_dict)
+        
+    
+def save(accelerator, model,model_path, optim_path, scheduler_path=None):
+        model_state_dict = accelerator.get_state_dict(model)
+        torch.save(model_state_dict, model_path)
+        print(model_path," 저장하였습니다.")
+       
+    
 def load_model(model,load_path):
+    
     state_dict = torch.load(load_path)
     new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
 
@@ -203,17 +221,22 @@ if __name__=="__main__":
     test_size = total_size-train_size
     print("train_size",train_size,"test_size",test_size)
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True,drop_last=True)
+    train_loader = DataLoader(train_dataset, num_workers=2, batch_size=64, shuffle=True,drop_last=True)
     test_batch_size=4
     test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True,drop_last=True)
     
-    
+    kwargs_handler = DistributedDataParallelKwargs(find_unused_parameters=True)
+    init_process_kwargs = InitProcessGroupKwargs(timeout = timedelta(seconds = 1800))
+    accelerator = Accelerator(
+                kwargs_handlers = [DEFAULT_DDP_KWARGS, init_process_kwargs],
+                log_with="wandb",gradient_accumulation_steps=2
+    )    
     # accelerator = Accelerator(gradient_accumulation_steps=8)
-    accelerator = Accelerator(log_with="wandb",gradient_accumulation_steps=2)
-    accelerator.init_trackers(
-        project_name="simple_lipnet", 
-        config={"dropout": 0.1, "learning_rate": 1e-2},
-    )
+    # accelerator = Accelerator(log_with="wandb",gradient_accumulation_steps=2,project_dir="/exp_logs/simple-lipnet")
+    # accelerator.init_trackers(
+    #     project_name="simple_lipnet", 
+    #     config={"dropout": 0.1, "learning_rate": 1e-2},
+    # )
     device=accelerator.device
     print(f"Using device: {device}")
     if device.type == 'cuda':
@@ -223,12 +246,15 @@ if __name__=="__main__":
     model=SimpleLipnet(vocab_size)
     # model.to(device)  # 모델을 적절한 device로 이동
     optimizer = Adam(model.parameters())
-    
+    scheduler=None
     train_loader, test_loader, model, optimizer = accelerator.prepare(
         train_loader, test_loader, model, optimizer
     )
+    if scheduler is not None:
+        scheduler = accelerator.prepare(scheduler)
+    accelerator.wait_for_everyone()
     test_loader_iter = iter(test_loader)
-
+    accelerator.init_trackers(project_name="simple_lipnet")
     decoder = CTCBeamDecoder(
         labels="abcdefghijklmnopqrstuvwxyz'?!123456789 #",
         model_path=None,
@@ -241,10 +267,11 @@ if __name__=="__main__":
         blank_id=0,
         log_probs_input=True
     )
-    # load_path="/exp_logs/checkpoints/model_epoch_52.pth"
-    # model=load_model(model,load_path)
+    # load_path="/exp_logs/simple-lipnet/s1_9.pth"
+    # load(model,accelerator,load_path,device)
+    
     num_epochs=100
-    for epoch in range(num_epochs):
+    for epoch in range(0,num_epochs):
         model.train()
         total_loss=0
         num=0
@@ -262,18 +289,22 @@ if __name__=="__main__":
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss/len(train_loader)}')
         
         
-        data, target = next(test_loader_iter)
+        try:
+            data, target = next(test_loader_iter)
+        except StopIteration:
+            test_loader_iter = iter(test_loader)
+            data, target = next(test_loader_iter)
         labels="abcdefghijklmnopqrstuvwxyz'?!123456789 #"
         beam_results, beam_scores, timesteps, out_lens=produce_sample(decoder,model, data)
         for i in range(test_batch_size):
             decoded = "".join(labels[n] for n in beam_results[i][0][:out_lens[i][0]])
             gt_decoded = "".join([labels[x] for x in target[i]])
-            print("prediction : ",decoded,"----- ground truth : ", gt_decoded)
+            print("prediction : ",decoded,"\tground truth : ", gt_decoded)
         
     
-        if epoch+1 % 10 == 1:
-            save_path = f'/exp_logs/checkpoints/simple_lipnet/epoch_{epoch+1}.pth'
-            save_model(model,save_path)
+        if (epoch + 1) % 3 == 0:
+            save_path = f'/exp_logs/simple-lipnet/s1_{epoch+1}.pth'
+            save(accelerator,model,save_path,None,None)
             
             
     accelerator.end_training()
